9e62391d6b78dc8b3136f12271a3e4ccb0783829
Kosei Moriyama <cou929@gmail.com>
2012-12-31 09:56:27 +0900

    use curl instead of w3m

--- tools/monitor.sample.sh
+++ tools/monitor.sample.sh
@@ -1,7 +1,7 @@
-LANG=ja_JP.UTF-8 w3m -dump http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml > /tmp/gjssg.1
+curl -s http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml -o /tmp/gjssg.1
   && diff -u /tmp/gjssg /tmp/gjssg.1 > /tmp/diff
   || cat /tmp/diff | mail cou929@example.com -s "[DIFF] Google JavaScript Style Guide"
   ; mv /tmp/gjssg.1 /tmp/gjssg
 
 # You can set this up to crontab like this:
-# 0 0 * * * LANG=ja_JP.UTF-8 w3m -dump http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml > /tmp/gjssg.1 && diff -u /tmp/gjssg /tmp/gjssg.1 > /tmp/diff || cat /tmp/diff | mail cou929@example.com -s "[DIFF] Google JavaScript Style Guide" ; mv /tmp/gjssg.1 /tmp/gjssg
+# 0 0 * * * curl -s http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml -o /tmp/gjssg.1 && diff -u /tmp/gjssg /tmp/gjssg.1 > /tmp/diff || cat /tmp/diff | mail cou929@example.com -s "[DIFF] Google JavaScript Style Guide" ; mv /tmp/gjssg.1 /tmp/gjssg
